5c10b905ae275942dc1c97135e9b1b68fb0e2fe7
ARSENIY KUSKOV <kuskovarseniy@gmail.com>
2025-12-10 20:59:55 +0100

    god final dll fix

--- menu.h
+++ menu.h
@@ -4,8 +4,8 @@
 #define _MENU_H_
 
 /* ============== DLL SETUP ============== */
-// comment or add a compiler flag if dynamic lib is needed
-#define MENULIB_STATIC
+// uncomment or add a compiler flag if static lib is needed
+// #define MENULIB_STATIC
 
 #if defined(MENULIB_STATIC)
 #define MENULIB_API // if static, no dll
